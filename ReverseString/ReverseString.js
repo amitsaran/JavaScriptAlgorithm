fe29900775026faa7b21138133ef8a9ee51a7b30
Amit Saran <amit_k_saran@yahoo.com>
2018-05-27 15:03:48 -0400

    -
    -

--- ReverseString/ReverseString.js
+++ ReverseString/ReverseString.js
@@ -1,5 +1,14 @@
-function reverseString(str){
-	return str.split('').reverse().join('');
+var ReverseString = {
+	reverseString: function(str){
+		return str.split('').reverse().join('');
+	},
+	reverseString1: function(str){
+		var reverse = '';
+		for(let character of str){
+			reverse = character + reverse;
+		}
+		return reverse;
+	}
 }
 
-module.exports = reverseString;
+module.exports = ReverseString;
--- ReverseString/test.js
+++ ReverseString/test.js
@@ -1,17 +1,21 @@
-const reverseString = require('./ReverseString');
+const ReverseString = require('./ReverseString');
 
 test('reverseString function is defined', () => {
-  expect(typeof reverseString).toEqual('function');
+  expect(typeof ReverseString.reverseString).toEqual('function');
+  expect(typeof ReverseString.reverseString1).toEqual('function');
 });
 
 test('reverses string \'abcd\'', () => {
-  expect(reverseString('abcd')).toEqual('dcba');
+  expect(ReverseString.reverseString('abcd')).toEqual('dcba');
+  expect(ReverseString.reverseString1('abcd')).toEqual('dcba');
 });
 
 test('reverses string \'  abcd\'', () => {
-  expect(reverseString('  abcd')).toEqual('dcba  ');
+  expect(ReverseString.reverseString('  abcd')).toEqual('dcba  ');
+  expect(ReverseString.reverseString1('  abcd')).toEqual('dcba  ');
 });
 
 test('reverses string \'123  abcd\'', () => {
-  expect(reverseString('123  abcd')).toEqual('dcba  321');
+  expect(ReverseString.reverseString('123  abcd')).toEqual('dcba  321');
+  expect(ReverseString.reverseString1('123  abcd')).toEqual('dcba  321');
 });
