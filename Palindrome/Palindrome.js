a17c41abf0810345dba29f78c989de4b04be0fff
Amit Saran <amit_k_saran@yahoo.com>
2018-05-27 15:44:16 -0400

    -
    -

--- Palindrome/Palindrome.js
+++ Palindrome/Palindrome.js
@@ -3,9 +3,14 @@ var Palindrome = {
 		return (str === str.split('').reduce((acumulator, element) => element + acumulator, ''));
 	},
 	isPalindrome1: function(str){
-		return (str === str.split('').reverse().join(''));
+		return str.split('').every((element, index) => {
+			return element === str[str.length - index - 1];
+		});
 	},
 	isPalindrome2: function(str){
+		return (str === str.split('').reverse().join(''));
+	},
+	isPalindrome3: function(str){
 		var reverse = '';
 		for(let character of str){
 			reverse = character + reverse;
--- Palindrome/test.js
+++ Palindrome/test.js
@@ -14,34 +14,40 @@ test('" aba" is not a palindrome', () => {
   expect(Palindrome.isPalindrome(' aba')).toBeFalsy();
   expect(Palindrome.isPalindrome1(' aba')).toBeFalsy();
   expect(Palindrome.isPalindrome2(' aba')).toBeFalsy();
+  expect(Palindrome.isPalindrome3(' aba')).toBeFalsy();
 });
 
 test('"aba " is not a palindrome', () => {
   expect(Palindrome.isPalindrome('aba ')).toBeFalsy();
   expect(Palindrome.isPalindrome1('aba ')).toBeFalsy();
-  expect(Palindrome.isPalindrome1('aba ')).toBeFalsy();
+  expect(Palindrome.isPalindrome2('aba ')).toBeFalsy();
+  expect(Palindrome.isPalindrome3('aba ')).toBeFalsy();
 });
 
 test('"greetings" is not a palindrome', () => {
   expect(Palindrome.isPalindrome('greetings')).toBeFalsy();
   expect(Palindrome.isPalindrome1('greetings')).toBeFalsy();
   expect(Palindrome.isPalindrome2('greetings')).toBeFalsy();
+  expect(Palindrome.isPalindrome3('greetings')).toBeFalsy();
 });
 
 test('"1000000001" a palindrome', () => {
   expect(Palindrome.isPalindrome('1000000001')).toBeTruthy();
   expect(Palindrome.isPalindrome1('1000000001')).toBeTruthy();
   expect(Palindrome.isPalindrome2('1000000001')).toBeTruthy();
+  expect(Palindrome.isPalindrome3('1000000001')).toBeTruthy();
 });
 
 test('"Fish hsif" is not a palindrome', () => {
   expect(Palindrome.isPalindrome('Fish hsif')).toBeFalsy();
   expect(Palindrome.isPalindrome1('Fish hsif')).toBeFalsy();
   expect(Palindrome.isPalindrome2('Fish hsif')).toBeFalsy();
+  expect(Palindrome.isPalindrome3('Fish hsif')).toBeFalsy();
 });
 
 test('"pennep" a palindrome', () => {
   expect(Palindrome.isPalindrome('pennep')).toBeTruthy();
   expect(Palindrome.isPalindrome1('pennep')).toBeTruthy();
   expect(Palindrome.isPalindrome2('pennep')).toBeTruthy();
+  expect(Palindrome.isPalindrome3('pennep')).toBeTruthy();
 });
